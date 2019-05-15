394432a6e7e5afea288a8d97fcb6e8648ef1024d
Lorenzo Vitali <93lorenzo@gmail.com>
2019-05-15 12:26:34 +0200

    Update main.py

--- simple_shapes_drawing/main.py
+++ simple_shapes_drawing/main.py
@@ -1,13 +1,21 @@
 import numpy as np
 import cv2
 
-#pip install opencv-python
-#https://realpython.com/python-opencv-color-spaces/
-#https://www.tutorialspoint.com/draw-geometric-shapes-on-images-using-python-opencv-module
-def draw_circle():
-    my_img = np.zeros((400, 400, 3), dtype="uint8")
+
+# https://realpython.com/python-opencv-color-spaces/
+# https://www.tutorialspoint.com/draw-geometric-shapes-on-images-using-python-opencv-module
+
+
+def draw_circle(sizes, colors, radius):
+    my_img = np.zeros(sizes, dtype="uint8")
     # creating circle
-    cv2.circle(my_img, (200, 200), 80, (0, 20, 200), 10)
+    cv2.circle(my_img, (200, 200), radius, colors, 50)
     cv2.imshow('Window', my_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+sizes = (400, 400, 3)
+colors = (0, 20, 200)
+radius = 50
+draw_circle(sizes, colors, radius)
